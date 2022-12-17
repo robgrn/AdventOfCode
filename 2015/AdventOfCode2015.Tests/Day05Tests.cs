07463cc25e1c590254df0b29fee4d54b0ec82ceb
Robbie Green <robgrn@users.noreply.github.com>
2022-12-17 23:10:05 +0000

    Add files for 2015 day 5

---
+++ 2015/AdventOfCode2015.Tests/Day05Tests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+using AdventOfCode2015.Day05;
+
+namespace AdventOfCode2015.Tests;
+
+public class Day05Tests
+{
+    public class PartOneTests
+    {
+        
+    }
+}
---
+++ 2015/AdventOfCode2015/Day05/Puzzle.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode2015.Day05;
+
+public class Puzzle
+{
+    private string[] input;
+
+    public Puzzle(string[] _input)
+    {
+        input = _input;
+    }
+
+    public void PartOne()
+    {
+
+    }
+}
